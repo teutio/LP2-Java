7d531be5c8048d5f2d430afcf9f31cc01a7bc9fb
teutio <121520061+teutio@users.noreply.github.com>
2024-10-21 12:38:27 -0300

    Update Main.java

--- L3EX1/src/Main.java
+++ L3EX1/src/Main.java
@@ -1,3 +1,10 @@
+/*1. Crie um vetor capaz de armazenar 50 números inteiros. Em seguida faça
+o seu preenchimento automático com os números de 101 a 150, ou seja,
+na posição número 0 ponha 101, na posição 1 ponha o número 102, e
+assim sucessivamente. Em seguida, exiba os valores deste vetor.
+*/
+
+
 import java.util.Arrays;
 
 public class Main {
